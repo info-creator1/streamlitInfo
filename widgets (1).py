4a6d33b5e0daeb550d706e5dcf39095838f5b939
info-creator1 <info@earlycoders.in>
2025-08-28 17:39:40 +0530

    Update widgets (1).py

--- widgets (1).py
+++ widgets (1).py
@@ -1,5 +1,5 @@
 
 import streamlit as st
 
-food = st.radio("choose your favourite food",["pizza","Burger","Pasta")
+food = st.radio("choose your favourite food",["pizza","Burger","Pasta"])
 st.write(f"You picked : {food}")
