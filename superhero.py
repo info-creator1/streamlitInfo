d1d2f90ca921c9a273d2d7c06fb50805bb538fc7
info-creator1 <info@earlycoders.in>
2025-09-02 17:10:27 +0530

    Add files via upload

---
+++ superhero.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.title("Superhero profile creator")
+
+name = st.text_input("Enter your superhero name")
+
+age = st.number_input("Enter your superhero's age")
+
+backstory = st.text_area("Write your superhero backstory")
+
+superpower = st.selectbox("Choose your main superpower", ["Flying","Invisibility","Mind Reading","Time Travel"])
+
+sidekicks = st.multiselect("Pick your superhero team", ["Robin","Iron Kid","shadow fox","Techno dog"])
+
+strength = st.slider("Rate your power level", 1,100,50)
+
+alignment = st.radio("Are you a hero or a villain?", ["Hero","villain"])
+
+if st.button("Generate Superhero story"):
+  st.subheader("Your superhero profile:")
+  st.write(f"Name: {name}")
+  st.write(f"Age: {age}")
+  st.write(f"Backstory: {backstory}")
+  st.write(f"Superpower: {superpower}")
+  st.write(f"sidekicks: {sidekicks}")
+  st.write(f"Strength: {strength}")
+  st.write(f"Alignment: {alignment}")
+
+
+
+
+
