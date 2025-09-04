f5f198c9e726ef4359d90ddf2ab45f5d67b0008b
info-creator1 <info@earlycoders.in>
2025-09-04 17:40:09 +0530

    Update title1.py

--- title1.py
+++ title1.py
@@ -1,4 +1,30 @@
 import streamlit as st
-file = st.file_uploader("Upload an image",type=["png","jpg","jpeg"])
-if file:
-    st.image(file)
+
+st.title("🎥My Photo Album")
+
+#upload multiple image files
+uploaded_files = st.file_uploader("Upload one or more images", type = ["png","jpg","jpeg"],
+                                  accept_multiple_files = True)
+
+if uploaded_files:
+  img_width = st.slider("Resize Image Width",100,500,250)
+
+  captions = []
+
+  for i, file in enumerate(uploaded_files):
+    caption = st.text_input(f"My Image no {i + 1}")
+    captions.append(caption)
+
+  #show uploaded images with respective captions
+  st.image(uploaded_files, caption = captions, width = img_width) 
+
+  st.success("Your photo album is displayed above")
+
+else:
+  st.info("Please upload one or more images to get started"))
+
+
+
+
+
+
