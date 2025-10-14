070758a1a6265593fea6301e521fcc51e1b33fab
info-creator1 <info@earlycoders.in>
2025-10-14 12:49:52 +0530

    Update VideoAudio.py

--- VideoAudio.py
+++ VideoAudio.py
@@ -1,19 +1,11 @@
 import streamlit as st
 
-st.set_page_config(page_title="3D Teddy Bear Viewer")
-st.title("🌈 3D Teddy Bear (Interactive)")
+st.title("🌈 3D Teddy Bear Viewer")
 
-# Path to your GLB file
-glb_file = "Untitled.glb"  # Put teddy.glb in the same folder as this script
-
-# HTML code using <model-viewer>
-html_code = f"""
+html_code = """
 <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
-
-<model-viewer src="{glb_file}" 
-              alt="Teddy Bear"
-              auto-rotate
-              camera-controls
+<model-viewer src="Untitled.glb" alt="Teddy Bear"
+              auto-rotate camera-controls
               background-color="#70BCD1"
               style="width: 100%; height: 600px;">
 </model-viewer>
