35d427966510279bb756316a212e0af2505a78d2
info-creator1 <info@earlycoders.in>
2025-10-14 13:16:10 +0530

    Update VideoAudio.py

--- VideoAudio.py
+++ VideoAudio.py
@@ -4,7 +4,7 @@ st.title("🌈 3D Teddy Bear Viewer")
 
 html_code = """
 <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
-<model-viewer src="Untitled.glb" alt="Teddy Bear"
+<model-viewer src="Teddy.glb" alt="Teddy Bear"
               auto-rotate camera-controls
               background-color="#70BCD1"
               style="width: 100%; height: 600px;">
